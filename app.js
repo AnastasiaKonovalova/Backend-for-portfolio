3570f9f93d45949969e9fe45dfe2115bbecd52c6
AnastasiaKonovalova <silentium901@mail.ru>
2019-03-29 12:36:13 +0300

    testing rendering pug + html

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cheerio = require("cheerio");
+
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, "public")));
+
+// const testErr = {
+//   status: "1234",
+//   stack: "1-2-3-4-5"
+// };
+
+// let rootHTML;
+// let errorHTML;
+// app.render("index.html", (err, html) => (rootHTML = html));
+// app.render(
+//   "error1",
+//   {
+//     message: "testest",
+//     error: testErr
+//   },
+//   (err, html) => {
+//     errorHTML = html;
+//   }
+// );
+
+// const fnHTML = cheerio.load(rootHTML);
+// fnHTML(".login__form").append(errorHTML);
+// const WholeHTML = fnHTML.html();
+// console.log("WholeHTML", WholeHTML);
+
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +58,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
+const cheerio = require("cheerio");
 var router = express.Router();
 
+// const app = require("../app");
+
+const renderPage = app => {
+  const testErr = {
+    status: "1234",
+    stack: "1-2-3-4-5"
+  };
+
+  let rootHTML;
+  let errorHTML;
+  app.render("index.html", (err, html) => (rootHTML = html));
+  app.render(
+    "error1",
+    {
+      message: "testest",
+      error: testErr
+    },
+    (err, html) => {
+      errorHTML = html;
+    }
+  );
+
+  const fnHTML = cheerio.load(rootHTML);
+  fnHTML(".login__form").append(errorHTML);
+  const WholeHTML = fnHTML.html();
+
+  return WholeHTML;
+  // console.log("WholeHTML", WholeHTML);
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  const html = renderPage(req.app);
+  res.send(html);
 });
+// router.get("/", function(req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
 module.exports = router;
